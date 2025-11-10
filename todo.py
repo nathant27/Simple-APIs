bbb8c12ba672d0d4c452ce210607898d1c62771e
nathant27 <nathan.tam27@gmail.com>
2025-11-10 01:28:04 -0800

    Added basic create item and get items methods

---
+++ todo.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class itemRequestFormat(BaseModel):
+    title: str
+    description: str
+
+idx = 0
+list_items = {} #for testing
+
+# user registration
+#responds with token for authentication if successful
+
+
+# user login 
+#responds with token for authentication if successful
+
+@app.post("/todos")
+# create to do item
+def create_item(item: itemRequestFormat):
+    global idx
+    list_items[idx] = {
+        "idx": idx,
+        "title": item.title,
+        "description": item.description
+    }
+    idx += 1
+
+    return 
+
+@app.get("/todos")
+def get_all_items():
+    print(list_items)
+    return list_items
+
+# update to do item
+
+# delete to do item
