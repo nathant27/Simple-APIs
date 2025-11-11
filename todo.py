5924fc89e9123fc222fd07c43e6293cb1e97234d
nathant27 <nathan.tam27@gmail.com>
2025-11-11 00:20:44 -0800

    Added update api method

--- todo.py
+++ todo.py
@@ -36,5 +36,15 @@ def get_all_items():
     return list_items
 
 # update to do item
+@app.put("/todos/{item_id}")
+def update_item(item_id: int, item: itemRequestFormat):
+    if item_id in list_items:
+        list_items[item_id].update({
+            "title": item.title,
+            "description": item.description
+        })
+        return list_items[item_id]
+        
+    return {"error": "Item not found"}
 
 # delete to do item
